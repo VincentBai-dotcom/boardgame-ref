d16b61374bf3d3d27f90bdcd9ddf9593b5cb9aaf
VincentBai-dotcom <butcheryyy@gmail.com>
2025-12-09 20:18:22 -0800

    add openapi to elysia

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,10 @@
+import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(openapi())
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
